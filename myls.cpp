5b6f45358051c12427c6873574ed8b9d77387152
Efimov Danil <danil.efimov.295@mail.ru>
2024-04-14 00:31:47 +0300

    get some of the colors right

--- myls.cpp
+++ myls.cpp
@@ -76,15 +76,34 @@ auto secToDate(auto wts) {
     return wts > six_months_ago ? std::format("{:%b %e %H:%M}", wts) : std::format("{:%b %e %Y}", wts);
 }
 
-void printColorfulFilename(auto filename, auto filetype, const std::filesystem::perms p) {
+void printColorfulFilename(auto filename, auto filestats) {
+    /* eval $(echo "no:global default;fi:normal file;di:directory;ln:symbolic link;pi:named pipe;so:socket;do:door;bd:block device;cd:character device;or:orphan symlink;mi:missing file;su:set uid;sg:set gid;tw:sticky other writable;ow:other writable;st:sticky;ex:executable;"|sed -e 's/:/="/g; s/\;/"\n/g')           
+{      
+  IFS=:     
+  for i in $LS_COLORS     
+  do        
+    echo -e "\e[${i#*=}m$( x=${i%=*}; [ "${!x}" ] && echo "${!x}" || echo "$x" )\e[m" 
+  done       
+} */
     using namespace std::filesystem;
     using std::filesystem::perms;
-    if (file_type::directory == filetype) {
-        std::cout << "\x1b[34m" << filename << "\033[0m\t\t";
+    auto p = filestats.permissions();
+    auto filetype = filestats.type();
+    constexpr auto endc = "\033[0m";
+    constexpr auto cyanc = "\x1B[36m";
+    constexpr auto redc = "\033[31m";
+    constexpr auto bluec = "\033[34m";
+    constexpr auto greenc = "\033[32m";
+    if (is_symlink(filestats)) {
+        std::cout << cyanc << filename << endc;
         return;
     }
-    if ((perms::owner_exec & p) != perms::none) {
-        std::cout << "\x1B[32m" << filename << "\033[0m\t\t";
+    if (is_directory(filestats)) {
+        std::cout << bluec << filename << endc;
+        return;
+    }
+    if ((perms::others_exec & p) != perms::none) {
+        std::cout << greenc << filename << endc;
         return;
     }
 //        case file_type::fifo:
@@ -99,7 +118,7 @@ void printColorfulFilename(auto filename, auto filetype, const std::filesystem::
 //            return 'c';
 //        case file_type::not_found:
 //            throw std::runtime_error{"lost a file, hmm"};
-            std::cout << filename;
+    std::cout << filename;
 
 }
 
@@ -113,9 +132,11 @@ void walkDirectory(const std::string& dirPath, Flags flags) {
         for(const auto fit : std::filesystem::directory_iterator(dirPath)) {
             auto fname = fit.path().filename().string();
             if (fname.find(' ') != std::string::npos) {
-                std::cout << fit.path().filename() << sep;
+                printColorfulFilename(fit.path().filename(), fit.status());
+                std::cout << sep;
             } else {
-                std::cout << fit.path().filename().string() << sep;
+                printColorfulFilename(fit.path().filename().string(), fit.status());
+                std::cout << sep;
             }
         }
         std::cout << "\n" << std::endl;
@@ -132,7 +153,11 @@ void walkDirectory(const std::string& dirPath, Flags flags) {
             std::cout << fit.path().filename().string() << "\n";
             continue;
         };
-        auto statcpp = fit.status();
+        auto filename = fit.path().filename().string();
+        if (filename.starts_with(".")) {
+            continue;
+        }
+        auto statcpp = fit.symlink_status();
         std::cout << getFileType(statcpp.type());
         printPermissions(statcpp.permissions());
         print(sb.st_nlink, 5, " ");
@@ -144,12 +169,14 @@ void walkDirectory(const std::string& dirPath, Flags flags) {
         }
         print(sb.st_size, 7, " ");
         auto wtime = std::filesystem::last_write_time(fit);
-        print(secToDate(wtime), 8, " ");
-        printColorfulFilename(fit.path().filename().string(), statcpp.type(), statcpp.permissions());
-//        std::cout << fit.path().filename().string() << " ";
+        print(secToDate(wtime), 12, " ");
+        printColorfulFilename(filename, statcpp);
         std::cout << " ";
         if (fit.is_symlink()) {
-            std::cout << "-> " << std::filesystem::read_symlink(fit).string();
+            auto linkname = std::filesystem::read_symlink(fit).string();
+            auto linkstats = std::filesystem::status(fit);
+            std::cout << "-> ";
+            printColorfulFilename(linkname, linkstats);
         }
         std::cout << "\n";
     }
